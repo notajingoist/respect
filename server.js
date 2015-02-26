08f50ec1cba1225aceefae00f6f234e042c83d6d
Jing Xiao <notajingoist@gmail.com>
2015-02-26 00:42:58 -0500

    separated navbar into its own reusable component

--- server.js
+++ server.js
@@ -19,36 +19,42 @@ app.set('view engine', 'jade');
 // home
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'R.E.S.P.E.C.T.'
+        title: 'R.E.S.P.E.C.T.',
+        home: 'active'
     });
 });
 
 app.get('/home', function(req, res) {
     res.render('index', {
-        title: 'R.E.S.P.E.C.T.'
+        title: 'R.E.S.P.E.C.T.',
+        home: 'active'
     });
 });
 
 app.get('/about', function(req, res) {
     res.render('about', {
-        title: 'R.E.S.P.E.C.T.'
+        title: 'R.E.S.P.E.C.T.',
+        about: 'active'
     });
 });
 
 app.get('/initiatives', function(req, res) {
     res.render('initiatives', {
-        title: 'R.E.S.P.E.C.T.'
+        title: 'R.E.S.P.E.C.T.',
+        initiatives: 'active'
     });
 });
 
 app.get('/calendar', function(req, res) {
     res.render('calendar', {
-        title: 'R.E.S.P.E.C.T.'
+        title: 'R.E.S.P.E.C.T.',
+        calendar: 'active'
     });
 });
 
 app.get('/gallery', function(req, res) {
     res.render('gallery', {
-        title: 'R.E.S.P.E.C.T.'
+        title: 'R.E.S.P.E.C.T.',
+        gallery: 'active'
     });
 });
